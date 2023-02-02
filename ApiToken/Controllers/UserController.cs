75c314c3efe691147922e47af2896de6cad8ac2b
Raptor76 <enjoy290178@gmail.com>
2023-02-02 21:22:58 +0100

    Initial Commit

---
+++ ApiToken/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ApiToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //For admin Only
+        [HttpGet]
+        [Route("Admins")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi you are an {currentUser.Role}");
+        }
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ ApiToken/Models/UserConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiToken.Models;
+
+public class UserConstants
+{
+    public static List<UserModel> Users = new()
+    {
+        new UserModel(){ Username="naeem",Password="naeem_admin",Role="Admin"}
+    };
+}
